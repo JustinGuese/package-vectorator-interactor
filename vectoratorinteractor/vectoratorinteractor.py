64497ea5bee52c6011e0db5f886db2093d88d34c
Justin Guese <guese.justin@gmail.com>
2025-03-19 15:19:16 +0100

    new interactor

--- vectoratorinteractor/vectoratorinteractor.py
+++ vectoratorinteractor/vectoratorinteractor.py
@@ -1,35 +1,78 @@
-import json
-import logging
+import enum
 from datetime import datetime
-from time import sleep
-from typing import List
+from typing import Dict, List, Optional
 
 import requests
 from fastapi import UploadFile
 from pydantic import BaseModel
+from sqlmodel import JSON, Column, Field, Relationship, SQLModel
 
 
-class Message(BaseModel):
-    content: str
-    is_ai: bool = True
+class SummaryStore(SQLModel, table=True):
+    id: str | None = Field(default=None, primary_key=True)
+    summary: str = Field(nullable=False)
 
 
-class SourceDocumentPD(BaseModel):
-    fullpath: str
-    filename: str
-    title: str
-    summary: str
-    content: str
-    url: str
-    url_expires_at: datetime
+class ProcessingState(enum.Enum):
+    PENDING = "PENDING"
+    PROCESSING = "PROCESSING"
+    DONE = "DONE"
+    FAILED = "FAILED"
 
 
-class ChatResponsePD(BaseModel):
-    question: str
-    answer: str | None = None
-    source_documents: List[SourceDocumentPD] = []
-    apporuser: str | None = None
-    project: str | None = None
+class Persona(enum.Enum):
+    agent = "agent"
+    user = "user"
+
+
+class QuestionsStore(SQLModel, table=True):
+    question: str = Field(primary_key=True, index=True, nullable=False)
+    message_id: Optional[int] = Field(default=None, foreign_key="chatmessage.id")
+    answer: Optional[str] = Field(default=None)
+    message: Optional["ChatMessage"] = Relationship()
+    project: str = Field(primary_key=True, index=True, nullable=False)
+    apporuser: str = Field(primary_key=True, index=True, nullable=False)
+    source_documents: Dict | None = Field(default_factory=dict, sa_column=Column(JSON))
+
+
+class Chat(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(default="new chat", nullable=False)
+    apporuser: str = Field(nullable=False)
+    project: str = Field(nullable=False)
+    created_at: datetime = Field(default_factory=datetime.utcnow, nullable=False)
+    processing_state: ProcessingState = Field(
+        nullable=False,
+        default=ProcessingState.DONE,
+    )
+    messages: list["ChatMessage"] = Relationship(back_populates="chat")
+
+
+class ChatMessage(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    chat_id: int | None = Field(default=None, foreign_key="chat.id")
+    chat: Chat | None = Relationship(back_populates="messages")
+    message: str = Field(nullable=False)
+    persona: Persona = Field(nullable=False)
+    created_at: datetime = Field(default_factory=datetime.utcnow, nullable=False)
+    source_documents: Dict | None = Field(default_factory=dict, sa_column=Column(JSON))
+
+
+class ChatWithMessages(BaseModel):
+    id: int
+    name: str
+    apporuser: str
+    project: str
+    created_at: datetime
+    processing_state: ProcessingState
+    messages: List[ChatMessage] = []
+
+
+class NewChat(BaseModel):
+    name: str
+    apporuser: str
+    project: str
+    messages: List[ChatMessage] = []
 
 
 class VectoratorInteractor:
@@ -79,69 +122,85 @@ class VectoratorInteractor:
         response.raise_for_status()
         return response.text.replace('"', "")
 
-    def __submitQuestion(
-        self,
-        question,
-        apporuser,
-        project_name,
-        messages: dict,
-    ) -> int:
-        url = (
-            self.vectoratorurl
-            + f"/question/{self.mainappname + "_" + apporuser}/{project_name}"
-        )
-        headers = {
-            "accept": "application/json",
-            "Content-Type": "application/x-www-form-urlencoded",
-        }
-        data = {
-            "question": question,
-            "message_history": json.dumps(messages),
-        }
-        response = requests.post(url, headers=headers, data=data)
-        response.raise_for_status()
-        return int(response.text)
-
-    # returns a chat_id
-    def submitQuestion(self, question, apporuser, project_name, messages: dict) -> int:
-        return self.__submitQuestion(question, apporuser, project_name, messages)
-
-    def getChatResponseForId(self, chat_id: int) -> ChatResponsePD:
-        url = self.vectoratorurl + f"/question/{chat_id}"
-        response = requests.get(url)
-        response.raise_for_status()
-        response = response.json()
-        return ChatResponsePD(**response)
-
-    def getChatResponseForIdWaitForFinish(self, chat_id) -> ChatResponsePD:
-        resp = self.getChatResponseForId(chat_id)
-        if resp.answer is None:
-            # still needs time
-            logging.debug("answer is not ready yet, wait")
-            sleep(2)
-            return self.getChatResponseForIdWaitForFinish(chat_id)
-        return resp
-
-    def question(
-        self, question, apporuser, project_name, messages: dict
-    ) -> ChatResponsePD:
-        chat_id = self.submitQuestion(question, apporuser, project_name, messages)
-        return self.getChatResponseForId(chat_id)
-
-    def isChatResponseReady(self, chat_id: int) -> bool:
-        resp = self.__getChatResponse(chat_id)
-        return resp.answer is not None
-
     def deleteProjectFromBackend(self, apporuser: str, project: str):
         url = self.vectoratorurl + f"/{self.mainappname + "_" + apporuser}/{project}"
         response = requests.delete(url)
         response.raise_for_status()
 
-    def quicksearch(self, apporuser: str, project: str, query: str) -> ChatResponsePD:
+    def quicksearch(self, apporuser: str, project: str, query: str) -> ChatMessage:
         url = (
             self.vectoratorurl
             + f"/quicksearch/{self.mainappname + "_" + apporuser}/{project}/{query}"
         )
         response = requests.get(url)
         response.raise_for_status()
-        return ChatResponsePD(**response.json())
+        return ChatMessage(**response.json())
+
+    ### Chat routes
+    def getChats(self, apporuser: str, project: str) -> List[ChatWithMessages]:
+        url = (
+            self.vectoratorurl + f"/chat/{self.mainappname + "_" + apporuser}/{project}"
+        )
+        response = requests.get(url)
+        response.raise_for_status()
+        return [ChatWithMessages(**chat) for chat in response.json()]
+
+    def getChat(self, apporuser: str, project: str, chat_id: int) -> ChatWithMessages:
+        url = (
+            self.vectoratorurl
+            + f"/chat/{self.mainappname + '_' + apporuser}/{project}/{chat_id}"
+        )
+        response = requests.get(url)
+        response.raise_for_status()
+        return ChatWithMessages(**response.json())
+
+    def createChat(self, chat: NewChat) -> ChatWithMessages:
+        url = self.vectoratorurl + f"/chat/"
+        response = requests.post(url, json=chat.model_dump())
+        response.raise_for_status()
+        return ChatWithMessages(**response.json())
+
+    def addMessage(
+        self, apporuser: str, project: str, message: ChatMessage
+    ) -> ChatWithMessages:
+        url = (
+            self.vectoratorurl
+            + f"/chat/message/{self.mainappname + '_' + apporuser}/{project}"
+        )
+        response = requests.put(url, json=message.model_dump())
+        response.raise_for_status()
+        return ChatWithMessages(**response.json())
+
+    def deleteChat(self, apporuser: str, project: str, chat_id: int):
+        url = (
+            self.vectoratorurl
+            + f"/chat/{self.mainappname + '_' + apporuser}/{project}/{chat_id}"
+        )
+        response = requests.delete(url)
+        response.raise_for_status()
+
+    # simplified question route
+    def question(
+        self, apporuser: str, project: str, question: str, chat_id: int = None
+    ) -> ChatWithMessages:
+        if chat_id is not None:
+            self.addMessage(
+                apporuser,
+                project,
+                ChatMessage(chat_id=chat_id, message=question, persona=Persona.user),
+            )
+            chat = self.getChat(apporuser, project, chat_id)
+        else:
+            newChat = NewChat(
+                apporuser=apporuser,
+                project=project,
+                messages=[ChatMessage(message=question, persona=Persona.user)],
+            )
+            chat = self.createChat(newChat)
+        maxtries = 30
+        crntTry = 0
+        while chat.processing_state != ProcessingState.DONE and crntTry < maxtries:
+            chat = self.getChat(apporuser, project, chat.id)
+            maxtries -= 1
+        assert chat.processing_state == ProcessingState.DONE
+        return chat
